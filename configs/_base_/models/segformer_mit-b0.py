be94a6c7b43fa2440beca416c30fceabbfbfd9b1
cottonlove <dbw2140@yonsei.ac.kr>
2023-07-12 00:20:55 +0900

    fix

--- configs/_base_/models/segformer_mit-b0.py
+++ configs/_base_/models/segformer_mit-b0.py
@@ -36,7 +36,7 @@ model = dict(
         norm_cfg=norm_cfg,
         align_corners=False,
         loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_name = "CE", loss_weight=1.0, class_weight = [0.1, 0.9])),
+            type='CrossEntropyLoss', use_sigmoid=False, loss_name = 'CE', loss_weight=1.0, class_weight = [0.1, 0.9])),
     # model training and testing settings
     train_cfg=dict(),
     test_cfg=dict(mode='whole'))
